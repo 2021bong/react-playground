4d9e6a85f29972b2122e6743605977ffc8cc7204
Bong <won950705@gmail.com>
2022-09-16 13:00:23 +0900

    ✨Add : css study 컴포넌트 추가 - input태그 checkbox와 css를 이용한 토글버튼 추가

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import CardList from './pages/kakao/CardList';
 import Monsters from './pages/UrlParameters/Monsters';
 import MonsterDetail from './pages/UrlParameters/MonsterDetail';
 import Pagination from './pages/Pagination/Users';
+import CssStudy from './pages/cssStudy/CssStudy';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/block' element={<StackBlock />} />
-        <Route path='/' element={<CardList />} />
+        <Route path='/Kakao' element={<CardList />} />
         <Route path='/monsters' element={<Monsters />} />
         <Route path='/monsters/detail/:id' element={<MonsterDetail />} />
         <Route path='/pagination' element={<Pagination />} />
+        <Route path='/' element={<CssStudy />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/cssStudy/CssStudy.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Toggle = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 100px;
+
+  .toggleSwitch,
+  .toggleButton {
+    transition: all 0.3s ease-in-out;
+  }
+
+  .toggleSwitch {
+    width: 6rem;
+    height: 3rem;
+    display: block;
+    position: relative;
+    border-radius: 2rem;
+    background-color: #fff;
+    box-shadow: 0 0 1rem 3px rgba(0 0 0 / 15%);
+    cursor: pointer;
+
+    .toggleButton {
+      width: 2.6rem;
+      height: 2.6rem;
+      position: absolute;
+      top: 50%;
+      left: 0.2rem;
+      transform: translateY(-50%);
+      border-radius: 50%;
+      background: #207cf5;
+    }
+  }
+
+  .toggleSwitch.active {
+    background: #207cf5;
+
+    .toggleButton {
+      /* 100% -> 끝위치, 2.8rem -> 버튼 크기 */
+      left: calc(100% - 2.8rem);
+      background: #fff;
+    }
+  }
+`;
+
+const CssStudy = (props) => {
+  const handleToggle = (e) => {
+    const toggleLabel = e.target.nextSibling;
+    toggleLabel.classList.toggle('active');
+  };
+
+  return (
+    <Toggle>
+      <input type='checkbox' id='toggle' hidden onClick={handleToggle} />
+      <label htmlFor='toggle' className='toggleSwitch'>
+        <span className='toggleButton'></span>
+      </label>
+    </Toggle>
+  );
+};
+
+export default CssStudy;
